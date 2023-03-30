792d4b9e0f7a64a6e22810433bed2621cbd3fd78
Felipe Olivares Campos <felipeolivares99@hotmail.com>
2023-03-29 22:23:46 -0300

    api back railway integration

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL =
+  process.env.REACT_APP_API_URL || "https://back-faltou.up.railway.app/";
 
 const api = axios.create({
   baseURL: API_URL,
